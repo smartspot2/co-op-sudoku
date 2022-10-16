dce687b3ee2d72b83ab46fa16dd2691e231869d3
jason z <jazhang00@gmail.com>
2022-10-16 01:11:51 -0700

    solvers and generators

--- sudokuop/sudoku/sudoku/sudoku_numpy.py
+++ sudokuop/sudoku/sudoku/sudoku_numpy.py
@@ -2,6 +2,27 @@ import numpy as np
 from numpy.random import default_rng
 import itertools
 
+class FlatBoard(object):
+    """
+    Stores the state of the board in a ROW x COL grid
+    """
+    N = 9
+    def __init__(self):
+        self.grid = np.zeros((self.N, self.N))
+    
+    def __str__(self):
+        return str(self.grid)
+
+    def __repr__(self):
+        return str(self.grid)
+
+    def __hash__(self):
+        return hash(str(self.grid))
+    
+    def __eq__(self, other):
+        return (self.grid == other.grid).all()
+
+
 class Board(object):
     """
     Stores the state of the board in a ROW x COL x CANDIDATE grid
@@ -69,12 +90,18 @@ class Board(object):
     def is_filled(self):
         return (self.grid.sum(axis=2) == 1).all()
 
+    def reset_cell(self, row, col):
+        self.grid[row, col] = np.ones(self.N)
+
     def set_cell(self, row, col, n):
         self.grid[row, col] *= 0
         self.grid[row, col, n] = 1
         assert self.cell_is_single(row, col)
     
-    def get_cell(self, row, col):
+    def get_cell_candidates(self, row, col):
+        return np.where(self.grid[row, col] == 1)[0]
+
+    def get_cell_value(self, row, col):
         assert self.cell_is_single(row, col)
         return np.where(self.grid[row, col] == 1)[0][0]
 
@@ -82,8 +109,10 @@ class Board(object):
         flat_grid = [[0] * self.N for _ in range(self.N)]
         for i, j in itertools.product(range(self.N), range(self.N)):
             if self.cell_is_single(i, j):
-                flat_grid[i][j] = self.get_cell(i, j) + 1
-        return flat_grid
+                flat_grid[i][j] = self.get_cell_value(i, j) + 1
+        flat_board = FlatBoard()
+        flat_board.grid = np.array(flat_grid)
+        return flat_board
 
     def copy(self):
         copy = Board()
@@ -92,6 +121,12 @@ class Board(object):
     
     def __str__(self):
         return str(self.flatten())
+    
+    def __hash__(self):
+        return hash(str(self.grid))
+    
+    def __eq__(self, other):
+        return (self.grid == other.grid).all()
 
 
 class Game(object):
@@ -122,9 +157,9 @@ class Game(object):
         self.views[1, 3:, :9] = 1
     
     def generate_toy_board(self):
-        from examples import solvable_1, unsolvable_1
+        from examples import solvable_1, solvable_2, unsolvable_1, unsolvable_2
         self.board = Board()
-        for idx, n in enumerate(solvable_1['values']):
+        for idx, n in enumerate(unsolvable_1['values']):
             i = idx // self.N
             j = idx % self.N
             if n > 0:
@@ -138,7 +173,7 @@ class Solver(object):
     
     def solved(self):
         def one_through_nine(subset):
-            subset = subset.reshape((-1, self.N))
+            subset = subset.reshape((-1, self.board.N))
             return (subset.sum(axis=0) == 1).all() and \
                    (subset.sum(axis=1) == 1).all()
 
@@ -158,6 +193,15 @@ class Solver(object):
                 return False
         return True
     
+    def remove_matching_candidates_in_view(self, i, j, view):
+        if view[i, j] and self.board.cell_is_single(i, j):
+            index = self.board.get_cell_value(i, j)
+            neighbors = self.board.get_neighbor_indices(i, j)
+            neighbors = [(x, y) for x, y in neighbors if view[x, y]]
+            num_removed = self.board.remove_from_cells(neighbors, index)
+            return num_removed
+        return 0
+    
     def iterative_solve(self):
         attempts = 0
         modified = True
@@ -165,12 +209,60 @@ class Solver(object):
             modified = False
             for view in self.views:
                 for i, j in itertools.product(range(self.N), range(self.N)):
-                    if view[i, j] and self.board.cell_is_single(i, j):
-                        index = self.board.get_cell(i, j)
-                        neighbors = self.board.get_neighbor_indices(i, j)
-                        num_removed = self.board.remove_from_cells(neighbors, index)
-                        if num_removed > 0:
-                            modified = True
+                    num_removed = self.remove_matching_candidates_in_view(i, j, view)
+                    modified = modified or (num_removed > 0)
             attempts += 1
         return self.solved()
+    
+    def backtrack_solve(self, i=0, j=0):
+        next_i = i + (j + 1) // self.N
+        next_j = (j + 1) % self.N
 
+        if i >= self.N:
+            return True
+        if self.board.cell_is_single(i, j):
+            return self.backtrack_solve(next_i, next_j)
+
+        candidates = list(range(self.N))
+        np.random.shuffle(candidates)
+        for candidate in candidates:
+            neighbors = self.board.get_neighbor_indices(i, j)
+            valid = True
+            for x, y in neighbors:
+                if self.board.cell_is_single(x, y):
+                    if self.board.get_cell_value(x, y) == candidate:
+                        valid = False
+            if valid:
+                self.board.set_cell(i, j, candidate)
+                solved = self.backtrack_solve(next_i, next_j)
+                if solved:
+                    return True
+                self.board.reset_cell(i, j)
+        return False
+    
+    def generate(self):
+        self.board = Board()
+        self.backtrack_solve()
+
+        solvable_boards = []
+        solvable_flats = []
+        solvable_boards.append(self.board.copy())
+        solvable_flats.append(self.board.flatten())
+
+        order = list(itertools.product(range(self.N), range(self.N)))
+        np.random.shuffle(order)
+
+        for i, j in order:
+            if self.board.cell_is_single(i, j):
+                prereset = self.board.copy()
+                self.board.reset_cell(i, j)
+                postreset = self.board.copy()
+
+                if self.iterative_solve():
+                    solvable_boards.append(postreset.copy())
+                    solvable_flats.append(postreset.flatten())
+                    self.board = postreset
+                else:
+                    self.board = prereset
+
+        return solvable_boards, solvable_flats
