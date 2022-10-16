47217f98b4649b36befcc82151928bcafede99b4
jason z <jazhang00@gmail.com>
2022-10-15 17:24:12 -0700

    separate board object

--- sudokuop/sudoku/sudoku/sudoku_numpy.py
+++ sudokuop/sudoku/sudoku/sudoku_numpy.py
@@ -1,10 +1,11 @@
 import numpy as np
+import itertools
 
-class Sudoku(object):
+
+class Board(object):
     N = 9
     def __init__(self):
         self.grid = np.ones((self.N, self.N, self.N))
-        self.views = []
     
     def get_row(self, i):
         return self.grid[i, :, :]
@@ -12,9 +13,36 @@ class Sudoku(object):
     def get_col(self, i):
         return self.grid[:, i, :]
     
+    def get_square(self, i, j):
+        assert 0 <= i < 3 and 0 <= i < 3
+        return self.grid[i:i+3, j:j+3, :]
+    
     def remove(self, row, col, candidates):
-        self.grid[row, col, candidates - 1] = 0
+        self.grid[row, col, candidates] = 0
     
     def set(self, row, col, n):
         self.grid[row, col] *= 0
         self.grid[row, col, n] = 1
+
+
+class Sudoku(object):
+    def __init__(self, num_players=2):
+        self.board = Board()
+        self.num_players = num_players
+        self.views = None
+
+    def generate_views(self, n, overlap=2, replace=True):
+        from numpy.random import default_rng
+
+        rng = default_rng()
+        self.views = np.zeros((n, self.N, self.N))
+
+        # every square must be seen
+        for i, j in itertools.product(range(self.N), range(self.N)):
+            view_idxs = rng.choice(n, size=overlap, replace=replace)
+            self.views[view_idxs, i, j] = 1
+
+        assert (np.sum(self.views, axis=0) > 0).all()
+    
+    def _generate_toy_view(self):
+        self.views = np.ones(())
